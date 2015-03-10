991275ca6dd362908e4e0574eb04b07d2e6f0a94
srph <seraphipod@gmail.com>
2015-03-11 06:33:34 +0800

    Added more test for `extend`.

--- test/extend.js
+++ test/extend.js
@@ -12,8 +12,12 @@ suite('extend', function() {
     assert.deepEqual(x, { hello: 'hi', hi: 'hello' });
   });
 
+  // {}, x:5, x:10, x:15
+  // x should be 15.
   test('should override keys from first to last (last obj\'s values remain)', function() {
     assert.deepEqual(extend({}, { hello: 'hi' }, { hello: 'hello' }), { hello: 'hello' });
     assert.deepEqual(extend({}, { hello: 'hi' }, { hello: 'hello' }, { hello: 'bae' }), { hello: 'bae' });
+
+    assert.deepEqual(extend({}, { hello: 'hi' }, { hello: 'hello', hi: 'ads', yolo :'swag' }, { hello: 'bae', yolo: 'sweg' }), { hello: 'bae', hi: 'ads', yolo: 'sweg'});
   });
 });
